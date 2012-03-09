453ae5edcc156aedf1f581e9559c8b5c548f111a
wangxian <xian366@126.com>
2012-03-09 15:46:40 +0800

    test

--- test/new-function.js
+++ test/new-function.js
@@ -12,7 +12,11 @@
 // console.log(new f({name:"wangxian"}));
 
 // use not eval
-ff = new Function('var name=12; if(1) console.log(1);else console.log(2); return 18;');
-console.log(ff());
+// ff = new Function('var name=12; if(1) console.log(1);else console.log(2); return 18;');
+// console.log(ff());
+
+// use two arguments
+fff = new Function('it,filter','zh', 'console.log(it,filter,zh); console.log(arguments)');
+fff(1,2,3);
 
 
