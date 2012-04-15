9a802ee3640de6e9ea22dd819f102bb46bb1a3a6
WangXian <xian366@126.com>
2012-04-15 22:37:20 +0800

    test mongous , a mongodb driver standard

---
+++ app/controllers/mongous.js
@@ -0,0 +1,12 @@
+/**
+ * module test mongous
+ * https://github.com/amark/mongous
+ */
+var controller = {};
+controller.index = function(){
+  
+  
+  app.res.end();
+};
+
+this.controller = controller;
