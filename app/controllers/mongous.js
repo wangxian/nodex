e692931f2cfd561cb6c842b07d4a701d8a6ddd08
WangXian <xian366@126.com>
2012-04-16 13:37:37 +0800

    test mongous

--- app/controllers/mongous.js
+++ app/controllers/mongous.js
@@ -4,8 +4,13 @@
  */
 var controller = {};
 controller.index = function(){
+  var $ = require("mongous").Mongous;
   
-  
+  $("test.test").save({name:'wx-2'});
+  $("test.test").find(function(ret){
+    console.dir(ret.documents);
+  })
+
   app.res.end();
 };
 
