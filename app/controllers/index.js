1bbbf7c7bc8b4553a20e7d8cc8f40240dbfdd346
WangXian <xian366@126.com>
2012-04-16 21:39:12 +0800

    test delay response.end

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,10 +10,11 @@ exports.controller = {
   
   'tmpl-cache-view': function(){
     dump( view._cache );
-    app.res.end();
+    // app.res.end();
+    setTimeout(function(){ app.res.end(); },10000);
   },
   'tmpl-cache-del': function(){
     view._cache = {};
-    app.res.end();
+    // app.res.end();
   }
 };
