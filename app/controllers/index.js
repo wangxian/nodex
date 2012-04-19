d9e87bdbd7e018b27ec90ddf6a19174a630308a2
WangXian <xian366@126.com>
2012-04-20 00:48:26 +0800

    wangxian commit.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,6 +5,6 @@
 exports.controller = {    
   'index': function(){
     app.res.setHeader("content-type", "text/html; charset=UTF-8");
-    app.render('index/index', {'name':'NodeX Start'});
+    app.render('index/index', {'title':'NodeX Setup'});
   }
 };
